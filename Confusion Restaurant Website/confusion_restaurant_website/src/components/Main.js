8abaf4615b929ab7fd533c43ed0cf449ee2d96a3
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-04-19 20:03:03 +0530

    developed the home page

--- Confusion Restaurant Website/confusion_restaurant_website/src/components/Main.js
+++ Confusion Restaurant Website/confusion_restaurant_website/src/components/Main.js
@@ -4,6 +4,9 @@ import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
 import Contact from './Contact';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { DISHES } from '../shared/dishes';
@@ -13,7 +16,9 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
@@ -22,11 +27,20 @@ class Main extends Component {
   }
 
   render() {
+    const HomePage = () => {
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
     return (
       <div>
         <Header />
         <Switch>
-          <Route path="/home" component={() => <Home />} />
+          <Route path="/home" component={HomePage} />
           <Route
             exact
             path="/menu"
