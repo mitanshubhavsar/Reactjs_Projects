fa2e987cbf193216bbb074db5b72f2771e7c072d
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-04-19 18:58:55 +0530

    Header and Footer

--- Confusion Restaurant Website/confusion_restaurant_website/src/components/Main.js
+++ Confusion Restaurant Website/confusion_restaurant_website/src/components/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './Menu';
+import Header from './Header';
+import Footer from './Footer';
 import DishDetail from './DishDetail';
 import { DISHES } from '../shared/dishes';
 
@@ -20,11 +21,7 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <Menu
           dishes={this.state.dishes}
           onClick={(dishId) => this.onDishSelect(dishId)}
@@ -36,6 +33,7 @@ class Main extends Component {
             )[0]
           }
         />
+        <Footer />
       </div>
     );
   }
