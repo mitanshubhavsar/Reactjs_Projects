8d1d1fb2a1a8b8acdb7280435b8069f9f3d2bf48
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-04-19 18:06:07 +0530

    Added Main Component

--- Confusion Restaurant Website/confusion_restaurant_website/src/App.js
+++ Confusion Restaurant Website/confusion_restaurant_website/src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import { DISHES } from './shared/dishes';
-import Menu from './components/Menu';
-import { useState } from 'react';
+
+import Main from './components/Main';
 
 function App() {
-  const [dishes, setDishes] = useState(DISHES);
-
   return (
     <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={dishes} />
+      <Main />
     </div>
   );
 }
