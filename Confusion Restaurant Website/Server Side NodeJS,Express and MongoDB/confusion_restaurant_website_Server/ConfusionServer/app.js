4228d7c2bc98a410d7b24163ea52d98f38f785f9
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-04-28 01:54:05 +0530

    Added Login,SignUp,Logout functions via sessions

--- Confusion Restaurant Website/Server Side NodeJS,Express and MongoDB/confusion_restaurant_website_Server/ConfusionServer/app.js
+++ Confusion Restaurant Website/Server Side NodeJS,Express and MongoDB/confusion_restaurant_website_Server/ConfusionServer/app.js
@@ -48,40 +48,23 @@ app.use(
   })
 );
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64')
-      .toString()
-      .split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-      req.session.user = 'admin';
-      next(); // authorized
-    } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    }
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
   } else {
-    if (req.session.user === 'admin') {
-      console.log('req.session: ', req.session);
+    if (req.session.user === 'authenticated') {
       next();
     } else {
       var err = new Error('You are not authenticated!');
-      err.status = 401;
-      next(err);
+      err.status = 403;
+      return next(err);
     }
   }
 }
@@ -93,9 +76,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
