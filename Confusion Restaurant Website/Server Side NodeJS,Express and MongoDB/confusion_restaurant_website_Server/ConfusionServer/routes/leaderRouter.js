e7d7588c0ccdcec78caab30d10044076cd74d9f9
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-05 00:04:52 +0530

    Authentication with passport json web token

--- Confusion Restaurant Website/Server Side NodeJS,Express and MongoDB/confusion_restaurant_website_Server/ConfusionServer/routes/leaderRouter.js
+++ Confusion Restaurant Website/Server Side NodeJS,Express and MongoDB/confusion_restaurant_website_Server/ConfusionServer/routes/leaderRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+var authenticate = require('../authenticate');
 
 const Leaders = require('../models/leaders');
 
@@ -21,7 +22,7 @@ leaderRouter
       )
       .catch((err) => next(err));
   })
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     Leaders.create(req.body)
       .then(
         (leader) => {
@@ -34,11 +35,11 @@ leaderRouter
       )
       .catch((err) => next(err));
   })
-  .put((req, res, next) => {
+  .put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /leaders');
   })
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Leaders.deleteMany({})
       .then(
         (resp) => {
@@ -65,11 +66,11 @@ leaderRouter
       )
       .catch((err) => next(err));
   })
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /leaders/' + req.params.leaderId);
   })
-  .put((req, res, next) => {
+  .put(authenticate.verifyUser, (req, res, next) => {
     Leaders.findByIdAndUpdate(
       req.params.leaderId,
       {
@@ -87,7 +88,7 @@ leaderRouter
       )
       .catch((err) => next(err));
   })
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Leaders.findByIdAndRemove(req.params.leaderId)
       .then(
         (resp) => {
@@ -120,7 +121,7 @@ leaderRouter
       )
       .catch((err) => next(err));
   })
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     Leaders.findById(req.params.leaderId)
       .then(
         (leader) => {
@@ -144,7 +145,7 @@ leaderRouter
       )
       .catch((err) => next(err));
   })
-  .put((req, res, next) => {
+  .put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end(
       'PUT operation not supported on /leaders/' +
@@ -152,7 +153,7 @@ leaderRouter
         '/feedback'
     );
   })
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Leaders.findById(req.params.leaderId)
       .then(
         (leader) => {
