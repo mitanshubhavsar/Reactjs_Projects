fdf44ad47b25b5aa004a85c6381e1e5ea58f6953
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-05 20:48:15 +0530

    Authentication and signup via Facebook

--- Confusion Restaurant Website/Server Side NodeJS,Express and MongoDB/confusion_restaurant_website_Server/ConfusionServer/config.js
+++ Confusion Restaurant Website/Server Side NodeJS,Express and MongoDB/confusion_restaurant_website_Server/ConfusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
   mongoUrl: 'mongodb://localhost:27017/conFusionDB',
+  facebook: {
+    clientId: '679786176291203',
+    clientSecret: 'af4c01d1d13fca9215ca4515ecffed4c',
+  },
 };
