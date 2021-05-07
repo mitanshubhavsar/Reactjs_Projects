7f8c4aa243670385b206db2f43d6097f0743976e
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-07 20:52:40 +0530

    Sending a Post request by axios

--- The Burger Builder App/the_burger_builder_app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ The Burger Builder App/the_burger_builder_app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENTS_PRICES = {
   salad: 0.5,
@@ -80,7 +81,25 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert('You Continue!');
+    // alert('You Continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Mitanshu Bhavsar',
+        address: {
+          street: 'testStreet',
+          zipCode: '3445345',
+          country: 'India',
+        },
+        email: 'test@test.com',
+      },
+      deliveryMethod: 'fastest',
+    };
+    axios
+      .post('./orders.json', order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
 
   render() {
