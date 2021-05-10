8ad40a040b815b08ad80e76f82b92c7fd68001d6
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-11 00:39:01 +0530

    Combining reducers burgerBuilder, order

--- The Burger Builder App/the_burger_builder_app/src/index.js
+++ The Burger Builder App/the_burger_builder_app/src/index.js
@@ -4,14 +4,21 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import reportWebVitals from './reportWebVitals';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/order';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer,
+});
+
 const store = createStore(
-  burgerBuilderReducer,
+  rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
 
