0329c8e6cad92a6d22d181bcca6c7d911cf86c3a
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-11 01:56:50 +0530

    Fetching Orders Via Redux

--- The Burger Builder App/the_burger_builder_app/src/store/reducers/order.js
+++ The Burger Builder App/the_burger_builder_app/src/store/reducers/order.js
@@ -1,7 +1,7 @@
 import * as actionTypes from '../actions/actionsTypes';
 
 const initialState = {
-  order: [],
+  orders: [],
   loading: false,
   purchased: false,
 };
@@ -27,9 +27,25 @@ const reducer = (state = initialState, action) => {
         ...state,
         loading: false,
         purchased: true,
-        order: state.order.concat(newOrder),
+        orders: state.order.concat(newOrder),
       };
     case actionTypes.PURCHASE_BURGER_FAIL:
+      return {
+        ...state,
+        loading: false,
+      };
+    case actionTypes.FETCH_ORDERS_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return {
+        ...state,
+        orders: action.orders,
+        loading: false,
+      };
+    case actionTypes.FETCH_ORDERS_FAIL:
       return {
         ...state,
         loading: false,
