1c59727a03fe1bae6f62dc6bf9d3a9e3fb33a088
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-10 13:28:05 +0530

    connect contactdata and checkout with redux store

--- The Burger Builder App/the_burger_builder_app/src/containers/Checkout/ContactData/ContactData.js
+++ The Burger Builder App/the_burger_builder_app/src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.css';
@@ -96,7 +97,6 @@ class ContactData extends Component {
 
   orderHandler = (event) => {
     event.preventDefault();
-    console.log(this.props.ingredients);
     const formData = {};
     for (let formElementIdentifier in this.state.orderForm) {
       formData[formElementIdentifier] = this.state.orderForm[
@@ -105,8 +105,8 @@ class ContactData extends Component {
     }
     this.setState({ loading: true });
     const order = {
-      ingredients: this.props.ingredients,
-      price: this.props.totalPrice,
+      ingredients: this.props.ings,
+      price: this.props.price,
       orderData: formData,
     };
     axios
@@ -194,4 +194,11 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStateToProps = (state) => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice,
+  };
+};
+
+export default connect(mapStateToProps)(ContactData);
