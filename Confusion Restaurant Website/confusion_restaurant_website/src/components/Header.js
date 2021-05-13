470d2a709ef86b7e1b040d64bc05f7aff5ecd537
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-13 18:34:11 +0530

    fix logo import

--- Confusion Restaurant Website/confusion_restaurant_website/src/components/Header.js
+++ Confusion Restaurant Website/confusion_restaurant_website/src/components/Header.js
@@ -17,6 +17,7 @@ import {
   Label,
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import logo from '../assets/images/logo.png';
 
 class Header extends Component {
   constructor(props) {
@@ -64,7 +65,7 @@ class Header extends Component {
             <NavbarToggler onClick={this.toggleNav} />
             <NavbarBrand className="mr-auto" href="/">
               <img
-                src="assets/images/logo.png"
+                src={logo}
                 height="30"
                 width="41"
                 alt="Ristorante Con Fusion"
