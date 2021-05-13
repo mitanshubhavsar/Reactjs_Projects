2b77fbf68f9a6f0c0845cd091689db0b05154e50
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-13 13:02:32 +0530

    Setting the right useEffect() Dependencies

--- The Burger Builder App/the_burger_builder_app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ The Burger Builder App/the_burger_builder_app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,9 +14,10 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 const BurgerBuilder = (props) => {
   const [purchasing, setPurchasing] = useState(false);
 
+  const { onInitIngredients } = props;
   useEffect(() => {
-    props.onInitIngredients();
-  }, []);
+    onInitIngredients();
+  }, [onInitIngredients]);
 
   const updatePurchaseState = (ingredients) => {
     const sum = Object.keys(ingredients)
