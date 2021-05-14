88f8ba2a0aa29e296e444d3fafe5a6730e16700b
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-15 02:31:33 +0530

    Initial Setup

---
+++ WhatsApp Clone/whatsapp-clone/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets Build a WhatsApp Clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ WhatsApp Clone/whatsapp-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBA2MWsUSPvVX1F0Wr4lISsE7Py5IMiL_o',
+  authDomain: 'whatsapp-clone-6ab4d.firebaseapp.com',
+  projectId: 'whatsapp-clone-6ab4d',
+  storageBucket: 'whatsapp-clone-6ab4d.appspot.com',
+  messagingSenderId: '840887516883',
+  appId: '1:840887516883:web:d54defda7596891f989e26',
+  measurementId: 'G-5ZSJSDK3MJ',
+};
