5000eaf3a7155e251170a7f82d152082869a75b5
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-15 13:21:17 +0530

    Added SidebarHeader Icons & SearchContainer

--- WhatsApp Clone/whatsapp-clone/src/Sidebar.js
+++ WhatsApp Clone/whatsapp-clone/src/Sidebar.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlined from '@material-ui/icons/SearchOutlined';
 import './Sidebar.css';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__header"></div>
-      <div className="sidebar__search"></div>
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
       <div className="sidebar__chats"></div>
     </div>
   );
