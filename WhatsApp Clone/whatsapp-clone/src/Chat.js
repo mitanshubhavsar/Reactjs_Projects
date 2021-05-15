14b976a0751c91889b0439233a8bdcd6fabb42db
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-15 20:38:35 +0530

    Adding & Styling Chat footer

--- WhatsApp Clone/whatsapp-clone/src/Chat.js
+++ WhatsApp Clone/whatsapp-clone/src/Chat.js
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import './Chat.css';
 import { Avatar, IconButton } from '@material-ui/core';
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import {
+  AttachFile,
+  MoreVert,
+  SearchOutlined,
+  InsertEmoticon,
+  Mic,
+} from '@material-ui/icons';
 
 function Chat() {
-  const [seed, setseed] = useState('');
+  const [input, setInput] = useState('');
+  const [seed, setSeed] = useState('');
 
   useEffect(() => {
-    setseed(Math.floor(Math.random() * 5000));
+    setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log(input);
+    setInput('');
+  };
 
   return (
     <div className="chat">
@@ -37,7 +50,21 @@ function Chat() {
           <span className="chat__timestamp">3:52pm</span>
         </p>
       </div>
-      <div className="chat__footer"></div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Type a message"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send the message
+          </button>
+        </form>
+        <Mic />
+      </div>
     </div>
   );
 }
