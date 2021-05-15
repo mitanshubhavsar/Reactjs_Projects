09e5d06ff10b30711546f2eb8d5a59eaef67f8c4
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-15 21:04:35 +0530

    Configure & add data on firebase database

--- WhatsApp Clone/whatsapp-clone/src/firebase.js
+++ WhatsApp Clone/whatsapp-clone/src/firebase.js
@@ -1,4 +1,4 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
 const firebaseConfig = {
   apiKey: 'AIzaSyBA2MWsUSPvVX1F0Wr4lISsE7Py5IMiL_o',
   authDomain: 'whatsapp-clone-6ab4d.firebaseapp.com',
@@ -8,3 +8,11 @@ const firebaseConfig = {
   appId: '1:840887516883:web:d54defda7596891f989e26',
   measurementId: 'G-5ZSJSDK3MJ',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
