0fffe17747e3aa80b8e1113b02d27205a45d3c5c
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-15 21:29:42 +0530

    Retrieve & add rooms to database

--- WhatsApp Clone/whatsapp-clone/src/Sidebar.js
+++ WhatsApp Clone/whatsapp-clone/src/Sidebar.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Avatar, IconButton } from '@material-ui/core';
 import { DonutLarge, Chat, MoreVert, SearchOutlined } from '@material-ui/icons';
 import SidebarChat from './SidebarChat';
 import './Sidebar.css';
+import db from './firebase';
 
 function Sidebar() {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    db.collection('rooms').onSnapshot((snapshot) =>
+      setRooms(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -29,10 +42,9 @@ function Sidebar() {
       </div>
       <div className="sidebar__chats">
         <SidebarChat addNewChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
+        {rooms.map((room) => (
+          <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+        ))}
       </div>
     </div>
   );
--- WhatsApp Clone/whatsapp-clone/src/SidebarChat.js
+++ WhatsApp Clone/whatsapp-clone/src/SidebarChat.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import './SidebarChat.css';
 import { Avatar } from '@material-ui/core';
+import db from './firebase';
 
-function SidebarChat({ addNewChat }) {
+function SidebarChat({ id, name, addNewChat }) {
   const [seed, setSeed] = useState('');
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
 
   const createChat = () => {
-    const roomName = prompt('Please enter name for chat');
+    const roomName = prompt('Please enter name for chat room!');
 
     if (roomName) {
       //do some clever database stuff...
+      db.collection('rooms').add({
+        name: roomName,
+      });
     }
   };
   return !addNewChat ? (
     <div className="sidebarChat">
       <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
       <div className="sidebarChat__info">
-        <h2>Room name</h2>
+        <h2>{name}</h2>
         <p>Last message...</p>
       </div>
     </div>
