077df3771a85a0bc4a4bc518efd9c2bfb5305d7c
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-17 02:00:24 +0530

    Added & Styled Product Component

--- Amazon Clone/amazon-clone/src/components/Home/Home.js
+++ Amazon Clone/amazon-clone/src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import Product from '../Product/Product';
 
 function Home() {
   return (
@@ -10,9 +11,9 @@ function Home() {
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt="amazon-banner"
         />
+
         <div className="home__row">
-          {/* Product */}
-          {/* Product */}
+          <Product />
         </div>
 
         <div className="home__row">
