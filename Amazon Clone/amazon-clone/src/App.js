3f2fdc67758f6d4b664cb188586c2646a5525b3f
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-16 22:36:30 +0530

    Added Home Component & styled Banner

--- Amazon Clone/amazon-clone/src/App.js
+++ Amazon Clone/amazon-clone/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     //BEM convention
     <div className="app">
       <Header />
-      {/* <Home /> */}
+      <Home />
     </div>
   );
 }
