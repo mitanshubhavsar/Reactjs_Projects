6db67640fa0edda286eaaddaa9ad525ae94ca127
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-18 16:31:26 +0530

    adding signout functionality

--- Amazon Clone/amazon-clone/src/components/Header/Header.js
+++ Amazon Clone/amazon-clone/src/components/Header/Header.js
@@ -3,9 +3,17 @@ import './Header.css';
 import { Search, ShoppingBasket } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 import { useStateValue } from '../../ContextAPI/StateProvider';
+import { auth } from '../../firebase';
 
 function Header() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  };
+
   return (
     <div className="header">
       <Link to="/home">
@@ -21,10 +29,12 @@ function Header() {
         <Search className="header__searchIcon" />
       </div>
       <div className="header__nav">
-        <Link to="/login">
-          <div className="header__option">
+        <Link to={!user && '/login'}>
+          <div onClick={handleAuthentication} className="header__option">
             <span className="header__optionLineOne">Hello Mitanshu</span>
-            <span className="header__optionLineTwo">Sign In</span>
+            <span className="header__optionLineTwo">
+              {user ? 'Sign Out' : 'Sign In'}
+            </span>
           </div>
         </Link>
         <div className="header__option">
