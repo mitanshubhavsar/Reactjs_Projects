5bc50339117b8beee9e1fc7f6adf120cf302164e
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-18 16:44:12 +0530

    Show username on Checkout & Header

--- Amazon Clone/amazon-clone/src/components/Checkout/Checkout.js
+++ Amazon Clone/amazon-clone/src/components/Checkout/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from '../../ContextAPI/StateProvider';
 import CheckoutProduct from '../CheckoutProduct/CheckoutProduct';
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -16,6 +16,7 @@ function Checkout() {
         />
 
         <div>
+          <h3>{user?.email}</h3>
           <h2 className="checkout__title">Your shopping Basket</h2>
           {basket.map((item) => (
             <CheckoutProduct
