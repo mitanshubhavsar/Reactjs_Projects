737c3355a63d1627ee28af192cbfb198d7199f36
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-05-20 13:33:48 +0530

    Added & styled Order Component

--- Amazon Clone/amazon-clone/src/components/Orders/Orders.js
+++ Amazon Clone/amazon-clone/src/components/Orders/Orders.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useStateValue } from '../../ContextAPI/StateProvider';
 import { db } from '../../firebase';
-
+import Order from '../Order/Order';
 function Orders() {
   const [{ basket, user }, dispatch] = useStateValue();
   const [orders, setOrders] = useState([]);
@@ -12,21 +12,28 @@ function Orders() {
         .doc(user?.uid)
         .collection('orders')
         .orderBy('created', 'desc')
-        .onSnapshot((snapshot) => {
+        .onSnapshot((snapshot) =>
           setOrders(
             snapshot.docs.map((doc) => ({
               id: doc.id,
               data: doc.data(),
             }))
-          );
-        });
+          )
+        );
     } else {
       setOrders([]);
     }
   }, [user]);
+
   return (
     <div className="orders">
       <h1>Your Orders</h1>
+
+      <div className="orders__order">
+        {orders?.map((order) => (
+          <Order order={order} />
+        ))}
+      </div>
     </div>
   );
 }
